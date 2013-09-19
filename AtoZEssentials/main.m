0366e9651a726873cd0226d4b254df583a9e3e8b
Alex Gray <alex@mrgray.com>
2013-09-19 13:17:59 -0400

    Initial commit.  Added BlockObservation from BlocksKit.

---
+++ AtoZEssentials/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  AtoZEssentials
+//
+//  Created by Alex Gray on 9/19/13.
+//  Copyright (c) 2013 Alex Gray. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	@autoreleasepool {	[NSApplication.sharedApplication run]; 	}    // execution never gets here ..
+	return 0;
+}
